194f27a7e0f0bd3a2ea66a127ff6afc7b77a74c6
darshansatapara <darshansatapara286@gmail.com>
2025-08-22 13:01:04 +0530

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://next-react-woad.vercel.app/"],
+    origin: "https://next-react-woad.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
